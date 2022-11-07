3cc0aa67db8200a2cb11321eaf226a9c1cc2861d
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-11-07 18:58:14 +0900

    Redux_Recap 1

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -5,16 +5,19 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => {
-    if (action.type === "ADD") {
-        return count + 1;
-    }else if (action.type === "MINUS"){
-        return count - 1;
-    }else {
-        return count;
+    switch (action.type){
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
-}
+};
 
 const onChange = () => {
     number.innerText = countStore.getState();
@@ -23,6 +26,6 @@ const onChange = () => {
 const countStore = createStore(countModifier);
 countStore.subscribe(onChange);
 
-add.addEventListener("click", () => countStore.dispatch({type: "ADD"}));
-minus.addEventListener("click", () => countStore.dispatch({type: "MINUS"}));
+add.addEventListener("click", () => countStore.dispatch({type: ADD}));
+minus.addEventListener("click", () => countStore.dispatch({type: MINUS}));
 
