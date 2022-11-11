7ffe5dd99cf1efebf31bfddca7571356d44927ac
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-11-11 11:51:28 +0900

    React_nodeJS_Auth 기능 만들기

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -24,7 +24,7 @@ app.get("/", (req, res) =>
   res.send("This is React-Redux Study! Opps...I am starving.")
 );
 
-app.post("/reqister", (req, res) => {
+app.post("/api/users/reqister", (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -38,7 +38,7 @@ app.post("/reqister", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 1. 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -68,6 +68,22 @@ app.post("/login", (req, res) => {
       .cookie("x_auth", user.token)
       .status(200)
       .json({ loginSuccess: true, userId: user._id });
+  });
+});
+
+// role 1 어드민   role 2 특정 부서 어드민
+// role 0 -> 일반유저   role 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
